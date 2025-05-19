f387bd30c71a0371fa0f97d42d0b7153bac2714c
kkjjkkll1111 <kkjjkkll1111@gmail.com>
2025-05-19 20:14:26 +0900

    v4.7 Graph(bfs)

--- week11.py
+++ week11.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 graph = [
     [0, 1, 1, 0, 0, 0, 0, 0],
     [1, 0, 0, 1, 0, 0, 0, 0],
@@ -16,5 +18,24 @@ def dfs(g, i, visited):
         if g[i][j] == 1 and not visited[j]:
             dfs(g, j, visited)
 
+def bfs(g, i, visited):
+    queue = deque()
+    visited[i] = 1
+    queue.append(i)
+
+    while queue:
+        j = queue.popleft()
+        for k in range(len(graph)):
+            if g[j][k] == 1 and not visited[k]:
+                visited[k] = 1
+                queue.append(k)
+        print(chr(ord('A') + j), end=' ')
+
+
+
 visited_dfs = [0 for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+
 dfs(graph, 6, visited_dfs)
+print()
+bfs(graph, 6, visited_bfs)
